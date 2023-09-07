1db2c046f597b180a8e4ee149d7f2782c904ba0a
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-09-07 19:37:17 +0530

    program

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+i=ord('A')#65
+n=ord('Z')#90
+while i<=n:
+    print(chr(i),end=" ")
+    i=i+1
---
+++ naturalnum.py
@@ -0,0 +1,5 @@
+n=int(input("Enter Any Number :"))
+i=int(input("Enter Start Number :"))
+while (i<=n):
+    print(i,end=" ")
+    i=i+1
