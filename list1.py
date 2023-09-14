6718d346c8a0bd4b66668bdf9b82064eb17a6632
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-09-14 19:05:59 +0530

    program

---
+++ list1.py
@@ -0,0 +1,7 @@
+list1=[]
+n= int(input("Enter Limit :"))
+for i in range(n):
+    num=int(input("Enter any number :"))
+    list1.append(num)
+
+print(list1)
---
+++ list2.py
@@ -0,0 +1,15 @@
+list1=[11,22,"ssi",True,8j,12.56]
+for i in list1:
+    print(i,end=" ")
+print()
+
+n=len(list1)
+for i in range(n):
+    print(list1[i],end=" ")
+
+
+print()
+i=0
+while i<n:
+    print(list1[i],end=" ")
+    i=i+1
